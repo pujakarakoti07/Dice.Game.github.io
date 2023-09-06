20d9337d842b56391c266fcf6a09f747b098acd6
pujakarakoti07 <144022134+pujakarakoti07@users.noreply.github.com>
2023-09-06 18:17:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
- function player1(){
-            var n=Math.floor(Math.random()*6)+1;
-            document.querySelector("img.player1").setAttribute("src","dice-six-faces-"+n+".png");            
-            var x=Math.floor(Math.random()*6)+1;
-            document.querySelector("img.player2").setAttribute("src","dice-six-faces-"+x+".png");
-            if(n<x){
-                document.querySelector("h1").innerHTML="PLAYER2 WINS!"
-                
-            }
-            else if(n>x){
-                document.querySelector("h1").innerHTML="PLAYER1 WINS!"
-            }
-            else{
-                document.querySelector("h1").innerHTML="DRAW!"
-            }
-        }      
+function player(){
+
+    var n=Math.floor(Math.random()*6)+1;
+    let music=new Audio("dice.mp3");
+    music.play();
+    document.querySelector("img.player1").setAttribute("src","dice-six-faces-"+n+".png");            
+    var x=Math.floor(Math.random()*6)+1;
+    document.querySelector("img.player2").setAttribute("src","dice-six-faces-"+x+".png");
+    if(n<x){
+        document.querySelector(".info").innerHTML="PLAYER2 WINS!"
+    }
+    else if(n>x){
+        document.querySelector(".info").innerHTML="PLAYER1 WINS!"
+
+
+    }
+    else{
+        document.querySelector(".info").innerHTML="DRAW!"
+
+    }
+}      
